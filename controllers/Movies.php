57a64ac0abbf885c73bfd7524312042a8972b3c6
MaxQuero <maxime.quero@gmail.com>
2015-04-02 17:27:23 +0200

    Ajout WebServices

--- controllers/Movies.php
+++ controllers/Movies.php
@@ -11,5 +11,3 @@ define('ALLOCINE_PARTNER_KEY', '100043982026');
 define('ALLOCINE_SECRET_KEY', '29d185d98c984a359e6e6f26a0474269');
 $allocine = new Allocine(ALLOCINE_PARTNER_KEY, ALLOCINE_SECRET_KEY);
 $result = $allocine->get(27415);
-$json_data = json_decode($result, true);
-echo $json_data['poster'];
